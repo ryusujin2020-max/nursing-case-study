34a7c57562e4554976cd25d1258223f8afcc54aa
sujin ryu <ryusujin2020@gmail.com>
2025-12-03 04:08:07 +0900

    Feature: 202512030408

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
-// tailwind.config.js
-
-const defaultTheme = require('tailwindcss/defaultTheme');
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  // ... (다른 설정 생략)
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
-        // 'nanum-square'라는 이름으로 폰트를 정의하고, 산세리프 폰트를 fallback으로 사용합니다.
-        'nanum-square': ['"Nanum Square Neo"', ...defaultTheme.fontFamily.sans],
+        sans: ['"Nanum Square Neo"', 'sans-serif'],
       },
     },
   },
-  // ...
+  plugins: [],
 }
